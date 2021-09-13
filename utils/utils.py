d2285d2933c010b3269a72a990f43a004e665d77
imad <imad.jamil@live.com>
2021-09-13 15:36:17 +0200

    fix pyright

--- utils/utils.py
+++ utils/utils.py
@@ -1,16 +1,19 @@
 import logging
+
+
 logger = logging.getLogger("utils-logger")
+
 
 class Utils:
     def __init__(self):
-        pass  
-	
-	def _logger(self):
-		if logger:
+        pass
+
+    def _logger(self):
+        if logger:
             return logger
         else:
-            raise Exception("logger is empty")  
-    
+            raise Exception("logger is empty")
+
     def _test(self):
-		self._logger.info("This is test")
-        #raise NotImplementedError
+        self._logger.info("This is test")
+        # raise NotImplementedError
